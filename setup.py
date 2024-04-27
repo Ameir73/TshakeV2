fa3f3b83f142cd36a35be156e82879b3ef29e6e4
Ameir73 <165747596+Ameir73@users.noreply.github.com>
2024-04-27 06:03:28 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ try:
   from config import *
   os.system('pm2 start bot.py --name {} --interpreter python3.7 --interpreter-args -u'.format(BOT_ID))
 except Exception as e:
-  API_ID = 793178
-  API_HASH = '9f4461079f30757ca0a4c23e14bd523f'
+  API_ID = 24952459
+  API_HASH = '9eabe8ce2ff616a6bf5a63d1f7905005'
 
   out ="""
 from pyrogram import enums
-API_ID = 793178
-API_HASH = '9f4461079f30757ca0a4c23e14bd523f'
+API_ID = 24952459
+API_HASH = '9eabe8ce2ff616a6bf5a63d1f7905005'
 """
   def Bot(TOKEN,method,data):
     url = "https://api.telegram.org/bot{}/{}".format(TOKEN,method)
